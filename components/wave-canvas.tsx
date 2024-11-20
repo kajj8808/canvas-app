44a21ba7444c493268230488dac0ee3145af1e39
강범창 <kajj8808@gangbeomchang-ui-MacBookAir.local>
2024-11-21 03:03:13 +0900

    📝 math.sin 을 이용해 기하학 1~-1 값 구하는 코드 부분 설명 추가

--- components/wave-canvas.tsx
+++ components/wave-canvas.tsx
@@ -8,32 +8,21 @@ export default function WaveCanvas() {
   // 아래 위로 왔다 갔다 하는 점 만들기.
   // 1. 일단 점을 중간에 찍어보자.
   // 2. 움직이게.. ?
+  // 3. 기하학 특성 가진 sin 사용 -> (0 = 0 , 90 = 1, 180 = 0 , 270 = -1 , 360 = 0)
+  // 450 -> 하나의 주기가 360 을 넘을 경우 450 - 360 = 90 => 90 = 1 이니 sin(450) -> 1
 
   useEffect(() => {
     if (canvasRef.current) {
       const canvas = canvasRef.current;
       const ctx = canvas.getContext("2d");
       if (!ctx) return;
-
-      const max = Math.random() * 100 + 150; // 움직이는 폭 최대. 위아래 값.
-      const frequency = 0.05; // sin 파동 주기 -> 작으면 느리게 파동이 흘러감. (animationSpeed 로 봐도 무방.)
-      let elapsed = 0; // sin 특징 1 ~ -1 인데. 값을 바꾸면서 위 아래 변경 하기 위해 사용. 코드에선 ++ 으로 값을 올려서 위 아래 반복.
-
-      const drawCircle = () => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        ctx.beginPath();
-        const y = canvas.height / 2 + Math.sin(elapsed * frequency) * max;
-        ctx.arc(canvas.width / 2, y, 30, 0, 2 * Math.PI);
-        ctx.fillStyle = "#f1f1f1";
-
-        ctx.fill();
-
-        elapsed++;
-        requestAnimationFrame(drawCircle);
-      };
-
-      requestAnimationFrame(drawCircle);
+      // 각도 (90 직각 180 반원.)
+      const angle = 90;
+      // Math.sin은 라디안 단위로 계산하기에 각도를 라디안으로 변환.
+      // 360도는 2PI 라디안 => 1도는 PI/180 즉 각도 * PI / 180 으로 라디안을 구할 수 있음.
+      const radians = (angle * Math.PI) / 180;
+      // sin설명은 위에 있음.
+      console.log(Math.sin(radians));
     }
   }, []);
 
